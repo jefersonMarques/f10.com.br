072f810bfb94ddf8fad63c4f8811649b1448e723
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-07-24 14:18:38 -0300

    Serve favicon fallback at runtime

--- src/routes/favicon.ico/+server.ts
+++ src/routes/favicon.ico/+server.ts
@@ -1,7 +1,5 @@
 import { redirect, type RequestHandler } from "@sveltejs/kit";
 
-export const prerender = true;
-
 export const GET: RequestHandler = () => {
   throw redirect(308, "/favicon.png");
 };
