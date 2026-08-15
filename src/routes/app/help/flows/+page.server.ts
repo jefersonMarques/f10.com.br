3d947a8fd6116aa9d6818897239bab2330981ddb
Jeferson Marques <34713644+jefersonMarques@users.noreply.github.com>
2026-08-15 13:15:03 -0300

    Fix help flows route typing

--- src/routes/app/help/flows/+page.server.ts
+++ src/routes/app/help/flows/+page.server.ts
@@ -1,4 +1,5 @@
-import { error, fail, redirect, type Actions, type PageServerLoad } from "@sveltejs/kit";
+import { error, fail, redirect, type Actions } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
 import { requireAppPermission } from "$lib/server/auth/authorization";
 import { hasPermission } from "$lib/server/auth/permissions";
 import {
